79a61dbf51263d63f89b8b83d1089c1ef26e96d0
omar khalaf <khalaf.omar20@gmail.com>
2025-06-12 10:29:29 +0300

    feat: created custom components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import { Hero } from "@/components";
+import { Hero, Searchbar, CustomFilter } from "@/components";
 
 export default function Home() {
   return (
       <main className="overflow-hidden">
         <Hero/>
+        <div className="mt-12 padding-x padding-y max-width" id="discover">
+           <div className="home__text-container">
+            <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+            <p>Discover the best car deals and services</p> 
+           </div>
+           <div className="home__filters">
+            <Searchbar />
+
+            <div className="home__filter-container">
+              <CustomFilter title="fuel" options={["all", "petrol", "diesel", "electric", "hybrid"]} />
+              <CustomFilter title="year" options={["2023", "2022", "2021", "2020", "2019"]} />
+              <CustomFilter title="transmission" options={["all", "automatic", "manual"]} />
+              <CustomFilter title="drive" options={["all", "4wd", "fwd", "rwd"]} />
+            </div>
+           </div>
+        </div>
       </main>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,8 @@
 import Hero from "./Hero";
 import CustomButton from './CustomButton';
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Searchbar from "./Searchbar";
+import CustomFilter from "./CustomFilter";
 
-export { Hero, CustomButton }; 
+export { Hero, CustomButton, NavBar, Footer, Searchbar, CustomFilter }; 
